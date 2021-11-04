2e259fb1fd8ee1ef008b6cdf5a10c9cbb0a299d7
Alexander Orlovsky <aaorlovsky@hotmail.com>
2021-11-04 21:27:30 +0300

    Fix logic

--- script.js
+++ script.js
@@ -87,6 +87,8 @@ function collisionDetection() {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("touchmove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
   if (e.key == "Right" || e.key == "ArrowRight") {
@@ -101,6 +103,13 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
+  }
+}
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 }
 
